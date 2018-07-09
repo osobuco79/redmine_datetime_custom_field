ec1945064bc46cd78d25d9e00fedfa2991af1233
osobuco79 <javifc79@gmail.com>
2018-07-09 15:49:00 +0200

    Update query_patch.rb
    works with date, datetime and text format

--- lib/redmine_datetime_custom_field/query_patch.rb
+++ lib/redmine_datetime_custom_field/query_patch.rb
@@ -3,37 +3,41 @@ require_dependency 'issue_query'
 
 class Query
 
-  def validate_query_filters
-    filters.each_key do |field|
-      if values_for(field)
-        case type_for(field)
-          when :integer
-            add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/\A[+-]?\d+(,[+-]?\d+)*\z/) }
-          when :float
-            add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/\A[+-]?\d+(\.\d*)?\z/) }
-          when :date, :date_past
-            case operator_for(field)
-              when "=", ">=", "<=", "><"
-                add_filter_error(field, :invalid) if values_for(field).detect {|v|
+  unless instance_methods.include?(:validate_query_filters_with_datetime_custom_field)
+    def validate_query_filters_with_datetime_custom_field
 
-                  ### CUSTOM BEGIN
-                  # add new valid date format
-                  v.present? && ((!v.match(/\A\d{4}-\d{2}-\d{2}(T\d{2}((:)?\d{2}){0,2}(Z|\d{2}:?\d{2})?)?\z/) && !v.match(/\A\d{2}\/\d{2}\/\d{4}(T\d{2}((:)?\d{2}){0,2}(Z|\d{2}:?\d{2})?)?\z/)) || parse_date(v).nil?)
-                  ### CUSTOM END
+      filters.each_key do |field|
+        if values_for(field)
+          case type_for(field)
+            when :integer
+              add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/\A[+-]?\d+(,[+-]?\d+)*\z/) }
+            when :float
+              add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/\A[+-]?\d+(\.\d*)?\z/) }
+            when :date, :date_past
+              case operator_for(field)
+                when "=", ">=", "<=", "><"
+                  add_filter_error(field, :invalid) if values_for(field).detect {|v|
 
-                }
-              when ">t-", "<t-", "t-", ">t+", "<t+", "t+", "><t+", "><t-"
-                add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/^\d+$/) }
-            end
+                    ### CUSTOM BEGIN
+                    # add new valid date format
+                    v.present? && ((!v.match(/\A\d{4}-\d{2}-\d{2}(T\d{2}((:)?\d{2}){0,2}(Z|\d{2}:?\d{2})?)?\z/) && !v.match(/\A\d{2}\/\d{2}\/\d{4}(T\d{2}((:)?\d{2}){0,2}(Z|\d{2}:?\d{2})?)?\z/)) || parse_date(v).nil?)
+                    ### CUSTOM END
+
+                  }
+                when ">t-", "<t-", "t-", ">t+", "<t+", "t+", "><t+", "><t-"
+                  add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/^\d+$/) }
+              end
+          end
         end
-      end
 
-      add_filter_error(field, :blank) unless
-          # filter requires one or more values
-          (values_for(field) and !values_for(field).first.blank?) or
-              # filter doesn't require any value
-              ["o", "c", "!*", "*", "t", "ld", "w", "lw", "l2w", "m", "lm", "y", "*o", "!o"].include? operator_for(field)
-    end if filters
+        add_filter_error(field, :blank) unless
+            # filter requires one or more values
+            (values_for(field) and !values_for(field).first.blank?) or
+                # filter doesn't require any value
+                ["o", "c", "!*", "*", "t", "ld", "w", "lw", "l2w", "m", "lm", "y", "*o", "!o"].include? operator_for(field)
+      end if filters
+    end
+    alias_method_chain :validate_query_filters, :datetime_custom_field
   end
 
   def quoted_time(time, is_custom_filter)
@@ -67,13 +71,13 @@ class Query
       if is_custom_filter && self.class.connection.adapter_name == "postgresql" && !Rails.env.test?
         s << ("to_timestamp(#{table}.#{field},'DD/MM/YYYY HH24:MI') > to_timestamp('%s','DD/MM/YYYY HH24:MI')" % [quoted_time(from, is_custom_filter)])
       else
-        if table.classify.constantize.columns_hash[field].type == :date
+        if table.classify.constantize.columns_hash[field].type == :date || table.classify.constantize.columns_hash[field].type == :datetime
           s << ("#{table}.#{field} > '%s'" % [quoted_time(from, is_custom_filter)])
         else
           s << ("STR_TO_DATE(#{table}.#{field},'%d/%m/%Y') > STR_TO_DATE('" + quoted_time(from, is_custom_filter) + "','%d/%m/%Y')")
         end
       end
-      ### Patch End
+      ### Patch End;
 
     end
     if to
@@ -88,7 +92,7 @@ class Query
       if is_custom_filter && self.class.connection.adapter_name == "postgresql" && !Rails.env.test?
         s << ("to_timestamp(#{table}.#{field},'DD/MM/YYYY HH24:MI') <= to_timestamp('%s','DD/MM/YYYY HH24:MI')" % [quoted_time(to, is_custom_filter)])
       else
-        if table.classify.constantize.columns_hash[field].type == :date
+        if table.classify.constantize.columns_hash[field].type == :date || table.classify.constantize.columns_hash[field].type == :datetime
           s << ("#{table}.#{field} <= '%s'" % [quoted_time(to, is_custom_filter)])
         else
           s << ("STR_TO_DATE(#{table}.#{field},'%d/%m/%Y') <= STR_TO_DATE('"+quoted_time(to, is_custom_filter)+"','%d/%m/%Y')")
